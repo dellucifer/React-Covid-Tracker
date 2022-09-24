49a13e91b3216f77222bbfd6f36157dfc01dbc04
Dellucifer <pg99285@gmail.com>
2022-09-24 22:39:15 +0530

    Updated the UI

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Line } from 'react-chartjs-2'
 
 function LineGraph() {
+    const [data, setData] = useState({})
+
+    useEffect(() =>{
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then(response => response.json())
+        .then(data => {
+
+        })
+    },[])
+
   return (
     <div>
-      
+      {/* <Line data options/> */}
+      <h3>i m graph</h3>
     </div>
   )
 }
--- src/Table.js
+++ src/Table.js
@@ -5,7 +5,7 @@ function Table({countries}) {
   return (
     <div className="table">
         {countries.map(({country, cases}) => (
-            <tr>
+            <tr key={country}>
                 <td>{country}</td>
                 <td><strong>{cases}</strong></td>
             </tr>
